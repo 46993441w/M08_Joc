a8a4c8b08bd4bdf4974c24b2cb9ff0d18d6f4010
david <46993441w@iespoblenou.org>
2016-04-28 01:25:00 +0200

    cau la birra i la agafa i el contador puja, el nivell també i va augmentant la velocitat de caiguda de la birra

--- gameMain.js
+++ gameMain.js
@@ -10,35 +10,97 @@ var joc;
         __extends(gameMain, _super);
         function gameMain() {
             _super.apply(this, arguments);
+            this.MAX_SPEED = 400; // pixels/second
+            this.velocitat_birra = 100;
+            this.ACCELERATION = 500; // pixels/second/second
+            this.DRAG = 600; // pixels/second/second
             this.VIDES = 3;
             this.contador = 0;
+            this.nivell = 1;
         }
         gameMain.prototype.create = function () {
             _super.prototype.create.call(this);
-            this.add.sprite(0, 0, 'fons');
+            this.crearFons();
             this.crearJugador();
+            this.crearVides();
             this.crearBirres();
             this.cursor = this.input.keyboard.createCursorKeys();
         };
         gameMain.prototype.update = function () {
             _super.prototype.update.call(this);
+            this.moureJugador();
+            this.moureBirra();
+            this.physics.arcade.overlap(this.jugador, this.birra, this.agafarBirra, null, this);
+        };
+        gameMain.prototype.crearFons = function () {
+            this.add.sprite(0, 0, 'fons');
+            var bmd = this.add.bitmapData(this.world.width, 45);
+            // dibuixar el rectangle que apareixerà al final del canvas
+            bmd.ctx.beginPath();
+            bmd.ctx.rect(0, 0, this.world.width, 45);
+            bmd.ctx.fillStyle = '#003300';
+            bmd.ctx.fill();
+            this.add.sprite(0, this.world.height - 43, bmd);
+            this.gameLevel = this.add.text(this.world.width - 120, this.world.height - 43, "Nivell: " + this.nivell, { font: '16px Arial', fill: '#ffffff' });
+            this.gamePuntuacio = this.add.text(this.world.width - 120, this.world.height - 23, "Puntuació: " + this.contador, { font: '16px Arial', fill: '#ffffff' });
         };
         gameMain.prototype.crearJugador = function () {
             this.jugador = this.add.sprite(this.world.centerX, 50, 'jugador');
             this.jugador.anchor.setTo(0.5, 0.5);
             this.jugador.checkWorldBounds = true;
             this.jugador.health = this.VIDES;
-            this.add.tween(this.jugador).to({ y: this.world.height - 100 }, 1000, Phaser.Easing.Bounce.Out, true);
+            this.add.tween(this.jugador).to({ y: this.world.height - 63 }, 1000, Phaser.Easing.Bounce.Out, true);
             this.physics.enable(this.jugador, Phaser.Physics.ARCADE); // activar la fisica del jugador
+            this.jugador.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+            this.jugador.body.collideWorldBounds = true;
+            this.jugador.body.drag.setTo(this.DRAG, this.DRAG); // x, y
+        };
+        gameMain.prototype.crearVides = function () {
+            this.jugadorVides = this.add.group();
+            for (var i = 0; i < this.VIDES; i++) {
+                var vida = this.add.sprite(i * 30, this.world.height - 1, 'jugador');
+                this.jugadorVides.add(vida);
+            }
+            this.jugadorVides.setAll('anchor.x', 0);
+            this.jugadorVides.setAll('anchor.y', 1);
         };
         gameMain.prototype.crearBirres = function () {
-            this.birra = this.add.group();
-            this.birra.enableBody = true;
-            this.birra.physicsBodyType = Phaser.Physics.ARCADE;
-            this.birra.createMultiple(20, 'birra');
-            this.birra.setAll('anchor.x', 0.5);
-            this.birra.setAll('anchor.y', 0.5);
-            this.birra.setAll('checkWorldBounds', true);
+            this.birra = this.add.sprite(this.rnd.integerInRange(0, this.world.width), 0, 'birra');
+            this.birra.anchor.setTo(0.5, 0);
+            this.birra.checkWorldBounds = true;
+            this.physics.enable(this.birra, Phaser.Physics.ARCADE); // activar la fisica del jugador
+            this.birra.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+            this.birra.body.collideWorldBounds = true;
+        };
+        gameMain.prototype.moureJugador = function () {
+            if (this.cursor.left.isDown ||
+                this.input.keyboard.isDown(Phaser.Keyboard.A)) {
+                this.jugador.body.acceleration.x = -this.ACCELERATION;
+                this.jugador.loadTexture('jugador_esquerra');
+            }
+            else if (this.cursor.right.isDown ||
+                this.input.keyboard.isDown(Phaser.Keyboard.D)) {
+                this.jugador.body.acceleration.x = this.ACCELERATION;
+                this.jugador.loadTexture('jugador_dreta');
+            }
+            else {
+                this.jugador.body.acceleration.x = 0;
+                this.jugador.loadTexture('jugador');
+            }
+        };
+        gameMain.prototype.moureBirra = function () {
+            this.birra.body.velocity.y = this.velocitat_birra;
+        };
+        gameMain.prototype.agafarBirra = function () {
+            this.contador += 1;
+            this.gamePuntuacio.setText("Puntuació: " + this.contador);
+            this.birra.body.y = 0;
+            this.birra.body.x = this.rnd.integerInRange(0, this.world.width);
+            if (this.contador % 10 == 0) {
+                this.nivell++;
+                this.velocitat_birra += 50;
+                this.gameLevel.setText("Nivell: " + this.nivell);
+            }
         };
         return gameMain;
     })(Phaser.State);
--- gameMain.ts
+++ gameMain.ts
@@ -5,18 +5,27 @@ module joc {
         game:Phaser.Game;
 
         private jugador:Phaser.Sprite;
-        private birra:Phaser.Group;
+        private birra:Phaser.Sprite;
         private cursor:Phaser.CursorKeys;
+        private jugadorVides:Phaser.Group;
+        private gameLevel:Phaser.Text;
+        private gamePuntuacio:Phaser.Text;
 
+        private MAX_SPEED = 400; // pixels/second
+        private velocitat_birra = 100;
+        private ACCELERATION = 500; // pixels/second/second
+        private DRAG = 600; // pixels/second/second
         private VIDES = 3;
         private contador = 0;
+        private nivell = 1;
+
 
         create():void {
             super.create();
 
-            this.add.sprite(0, 0, 'fons');
-
+            this.crearFons();
             this.crearJugador();
+            this.crearVides();
             this.crearBirres();
             this.cursor = this.input.keyboard.createCursorKeys();
         }
@@ -24,6 +33,27 @@ module joc {
         update():void {
             super.update();
 
+            this.moureJugador();
+            this.moureBirra();
+
+            this.physics.arcade.overlap(this.jugador, this.birra, this.agafarBirra, null, this);
+        }
+
+        private crearFons():void {
+            this.add.sprite(0, 0, 'fons');
+
+            var bmd = this.add.bitmapData(this.world.width,45);
+
+            // dibuixar el rectangle que apareixerà al final del canvas
+            bmd.ctx.beginPath();
+            bmd.ctx.rect(0,0,this.world.width,45);
+            bmd.ctx.fillStyle = '#003300';
+            bmd.ctx.fill();
+
+            this.add.sprite(0, this.world.height-43, bmd);
+
+            this.gameLevel = this.add.text(this.world.width - 120,this.world.height - 43, "Nivell: " + this.nivell,{font: '16px Arial', fill: '#ffffff'})
+            this.gamePuntuacio = this.add.text(this.world.width - 120,this.world.height - 23, "Puntuació: " + this.contador,{font: '16px Arial', fill: '#ffffff'})
         }
 
         private crearJugador():void {
@@ -32,20 +62,64 @@ module joc {
             this.jugador.checkWorldBounds = true;
             this.jugador.health = this.VIDES;
 
-            this.add.tween(this.jugador).to({y: this.world.height - 100}, 1000, Phaser.Easing.Bounce.Out, true);
+            this.add.tween(this.jugador).to({y: this.world.height - 63}, 1000, Phaser.Easing.Bounce.Out, true);
             this.physics.enable(this.jugador, Phaser.Physics.ARCADE); // activar la fisica del jugador
+
+            this.jugador.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+            this.jugador.body.collideWorldBounds = true;
+            this.jugador.body.drag.setTo(this.DRAG, this.DRAG); // x, y
+        }
+
+        private crearVides():void {
+            this.jugadorVides = this.add.group();
+            for(var i = 0; i < this.VIDES; i++){
+                var vida = this.add.sprite(i*30, this.world.height - 1, 'jugador');
+                this.jugadorVides.add(vida);
+            }
+            this.jugadorVides.setAll('anchor.x', 0);
+            this.jugadorVides.setAll('anchor.y', 1);
         }
 
         private crearBirres():void {
-            this.birra = this.add.group();
-            this.birra.enableBody = true;
-            this.birra.physicsBodyType = Phaser.Physics.ARCADE;
-            this.birra.createMultiple(20, 'birra');
+            this.birra = this.add.sprite(this.rnd.integerInRange(0,this.world.width), 0, 'birra');
+            this.birra.anchor.setTo(0.5, 0);
+            this.birra.checkWorldBounds = true;
 
-            this.birra.setAll('anchor.x', 0.5);
-            this.birra.setAll('anchor.y', 0.5);
-            this.birra.setAll('checkWorldBounds', true);
+            this.physics.enable(this.birra, Phaser.Physics.ARCADE); // activar la fisica del jugador
 
+            this.birra.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+            this.birra.body.collideWorldBounds = true;
+        }
+
+        private moureJugador():void {
+            if (this.cursor.left.isDown ||
+                this.input.keyboard.isDown(Phaser.Keyboard.A)) {
+                this.jugador.body.acceleration.x = -this.ACCELERATION;
+                this.jugador.loadTexture('jugador_esquerra');
+            } else if (this.cursor.right.isDown ||
+                this.input.keyboard.isDown(Phaser.Keyboard.D)) {
+                this.jugador.body.acceleration.x = this.ACCELERATION;
+                this.jugador.loadTexture('jugador_dreta');
+            } else {
+                this.jugador.body.acceleration.x = 0;
+                this.jugador.loadTexture('jugador');
+            }
+        }
+
+        private moureBirra():void {
+            this.birra.body.velocity.y = this.velocitat_birra;
+        }
+
+        private agafarBirra():void {
+            this.contador += 1;
+            this.gamePuntuacio.setText("Puntuació: " + this.contador);
+            this.birra.body.y = 0;
+            this.birra.body.x = this.rnd.integerInRange(0,this.world.width);
+            if(this.contador%10 == 0){
+                this.nivell++;
+                this.velocitat_birra += 50;
+                this.gameLevel.setText("Nivell: " + this.nivell);
+            }
         }
     }
 }
